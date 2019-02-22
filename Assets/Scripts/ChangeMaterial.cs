ff278c63e9f439ab5f78e8be9815b59175c30118
JincomVR <donovan@jincom.co.za>
2019-02-22 13:54:43 +0200

    BaseSceneCommit
    committing the base file to the repo via Unity to see if the errors persist. This verion is the v03, which is the package that was saved out by selecting the scene and creating a package directly from that.

---
+++ Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour {
+
+    public Material[] material;
+    public Material defaultColour;
+    public Material GreenGlow;
+    public Material RedGlow;
+
+    Renderer rend;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = defaultColour;
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            rend.sharedMaterial = GreenGlow;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            rend.sharedMaterial = RedGlow;
+        }
+    }
+
+
+   // void OnCollisionEnter(Collision col)
+   // {
+   //     if (col.gameObject.tag == "MtlChangeObject")
+   //     {
+   //         rend.sharedMaterial = material[1];
+   //     }
+  //      else
+  //      {
+  //          rend.sharedMaterial = material[2];
+  //      }
+  //  }
+}
---
+++ Assets/Scripts/ChangeMaterialOnCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterialOnCollision : MonoBehaviour {
+
+    public Material[] material;
+    Renderer rend;
+
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+		
+	}
+
+   void OnCollisionEnter(Collision col)
+    {
+       if (col.gameObject.tag == "MtlChangeObject")
+        {
+            rend.sharedMaterial = material[1];
+        }
+        else
+        {
+            rend.sharedMaterial = material[2];
+        }
+    }
+
+}
